f31e1fd0a2702a147e1d7b82e1cdb44be72a4cc8
Berkant Karaca <berkantkaracatr@gmail.com>
2025-11-26 10:13:09 +0300

    update

--- OracleSQL/Ders7.sql
+++ OracleSQL/Ders7.sql
@@ -1,9 +1,6 @@
 /*
     Cas Expression => Değer döndürür. Insert, update vb kodları çalıştıramaz
     Case Statement => Koşula göre işlemi yapar
-
+    
     Coalesce => Aldığı parametrelerden ilk null olmayan değeri döndürür. Tüm parametreler null ise null döner
-
-
-
 */
